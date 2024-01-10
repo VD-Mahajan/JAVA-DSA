959d9d4eedc97f41dedbfbae5d2252be05dfe427
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-10 23:50:07 +0530

    Initial commit

--- Array/NumberFrequency.java
+++ Array/NumberFrequency.java
@@ -1,5 +1,25 @@
 package Array;
 
-public class NumberFrequency {
+import java.util.HashMap;
+import java.util.Map;
 
+public class NumberFrequency {
+	
+	public static void main(String[] args) {
+		int[] arr = new int[] {1,2,3,4,1,2,3,4,2,3,4,5,6};
+		
+		HashMap<Integer,Integer> hm = new HashMap<Integer, Integer>();
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(!hm.containsKey(arr[i])) {
+				hm.put(arr[i],1);
+			}else {
+				hm.put(arr[i],hm.get(arr[i])+1);
+			}
+		}
+		
+		for(Map.Entry<Integer,Integer> entry : hm.entrySet()) {
+			System.out.println(entry.getKey()+"   :   "+entry.getValue());
+		}
+	}
 }
